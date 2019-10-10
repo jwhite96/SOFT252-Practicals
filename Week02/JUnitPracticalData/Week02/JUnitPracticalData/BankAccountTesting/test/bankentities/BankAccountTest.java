b4e2148365ce5d747db807e4a9ef561f442d5c91
James White <44773230+jwhite96@users.noreply.github.com>
2019-10-10 12:50:57 +0100

    Add files via upload

---
+++ Week02/JUnitPracticalData/Week02/JUnitPracticalData/BankAccountTesting/test/bankentities/BankAccountTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankentities;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class BankAccountTest {
+    
+    public BankAccountTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testDepositMoney() {
+    }
+
+    @Test
+    public void testWithdrawMoney() {
+    }
+
+    @Test
+    public void testWithdrawalIsOK() {
+    }
+
+    @Test
+    public void testGetBalance() {
+    }
+
+    @Test
+    public void testGetHolder() {
+    }
+
+    @Test
+    public void testGetOverdraft() {
+    }
+
+    @Test
+    public void testSetOverdraft() {
+    }
+    
+}
---
+++ Week02/JUnitPracticalData/Week02/JUnitPracticalData/MyYear/src/classification/StageResults.java
@@ -0,0 +1,103 @@
+package classification;
+
+public class StageResults {
+    private double totalMarks;
+    private int totalCredits;
+    private double stage2Average;
+    private final int MAXCREDITS = 120;
+
+    /*
+     * Constructor. Initialises the instrance variables with zero.
+     */
+    public StageResults() {
+        totalMarks = 0.0;
+        totalCredits = 0;
+        stage2Average = 0.0;
+    }
+
+    /*
+     * Returns the stage 2 average.
+     */
+    public double getStage2Average() {
+        return stage2Average;
+    }
+
+    /*
+     * Returns the total credits added so far.
+     */
+    public int getTotalCredits() {
+        return totalCredits;
+    }
+
+    /*
+     * Returns the total mark added so far
+     */
+    public double getTotalMarks() {
+        return totalMarks;
+    }
+
+    /*
+     * takes the stage 2 average as a parameter and places it
+     * in the instance variable.
+     */
+    public void setStage2Average(double stage2Average) {
+        this.stage2Average = stage2Average;
+    }
+    
+    /*
+     * Returns TRUE of 120 credits have been entered, FALSE otherwise.
+     */
+    public boolean isComplete() {
+        return (totalCredits == MAXCREDITS);
+    }
+    
+    /*
+     * Resets the instance variables to zero, destroying any existing
+     * values.
+     */
+    public void resetValues() {
+        totalMarks = 0.0;
+        totalCredits = 0;
+        stage2Average = 0.0;
+    }
+    
+    public void addModuleMark(int credits, double mark){
+        totalCredits += credits;
+        totalMarks += mark * (credits / 10);
+    }
+    
+    public double calculateAverageSoFar(){
+        double average;
+        
+        average = totalMarks / (totalCredits / 10.0);
+        average = Math.round(average * 100) / 100.0;
+        
+        return average;
+    }
+    
+    public String predictClass(){
+        double overallAverage = calculateAverageSoFar();
+        String degree;
+        
+        if (totalCredits != 0)
+            overallAverage = Math.round(overallAverage * 0.7 * 100) / 100.0
+                    + Math.round(stage2Average * 0.3 * 100) /100.0;
+        
+        if (totalCredits < MAXCREDITS)
+            degree = "Insufficient Credits";
+        else if (overallAverage == 0)
+            degree = "No Marks";
+        else if (overallAverage < 40)
+            degree = "FAIL";
+        else if (overallAverage < 50)
+            degree = "3rd";
+        else if (overallAverage < 60)
+            degree = "Upper 2nd";
+        else if (overallAverage < 70)
+            degree = "Upper 2nd";
+        else
+            degree = "1st";
+        
+        return degree;
+    }    
+}
