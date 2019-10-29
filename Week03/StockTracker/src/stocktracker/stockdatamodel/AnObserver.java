ea46892bf667b2794e917392b26efda9f1dba9b9
(s) James White 1 <james.white-12@students.plymouth.ac.uk>
2019-10-29 12:27:26 +0000

    StockTracker Update WK5

--- Week03/StockTracker/src/stocktracker/stockdatamodel/AnObserver.java
+++ Week03/StockTracker/src/stocktracker/stockdatamodel/AnObserver.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stocktracker.stockdatamodel;
+import utilities.IObserver;
 
-/**
- *
- * @author jwhite12
- */
-public interface AnObserver {
+public class AnObserver implements IObserver {
     
+    @Override
+    public void update(){
+        System.out.println("State Change Detected");
+    }
 }
